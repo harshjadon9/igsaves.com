b054c80504e2c68d2370e02df79df2051f8bc71f
harshjadon9 <62889318+harshjadon9@users.noreply.github.com>
2021-06-06 03:26:06 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,35 @@
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+const cacheName = 'igsaves-cache-v1';
+const resourcesToPrecache = [
+    '/',
+    '/img/min/logo.png',
+];
+
+self.addEventListener('install', event => {
+    console.log('✔ SW Installed! ');
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(resourcesToPrecache);
+            })
+            .then(caches.keys().then((keyList) => {
+                    return Promise.all(keyList.map((key) => {
+                        if (cacheName.indexOf(key) === -1) {
+                            return caches.delete(key);
+                        }
+                    }));
+                })
+            )
+    );
+});
+
+self.addEventListener('activate', event => {
+    console.log('🚀 Activate SW!');
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request)
+        .then(cachedResponse => {
+            return cachedResponse || fetch(event.request);
         })
     );
-  });
+});
